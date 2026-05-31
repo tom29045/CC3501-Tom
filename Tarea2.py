5fd6b41f893f7731efa5931888701cb4d917592e
tom29045 <tomascanales8deabril@gmail.com>
2026-05-31 22:10:03 +0000

    hola

--- Tarea2.py
+++ Tarea2.py
@@ -8,6 +8,7 @@ from pyglet import clock
 
 import sys, os
 import numpy as np
+import grafica.transformations as tr
 
 #MODULOS (cuidado con las rutas)
 sys.path.append(os.path.dirname(os.path.dirname((os.path.dirname(__file__)))))
@@ -48,48 +49,69 @@ if __name__ == "__main__":
 
 
 # Cambio de color a textura
-    vert_source = """
-#version 330
+    vert_source = """ 
+    #version 330
 
-in vec3 position;
-in vec2 texCoord; 
+    in vec3 position;
+    in vec2 texCoord; 
 
-out vec2 fragTexCoord; 
+    out vec2 fragTexCoord; 
 
-uniform mat4 u_model = mat4(1.0);
-uniform mat4 u_view = mat4(1.0);
-uniform mat4 u_projection = mat4(1.0);
+    uniform mat4 u_model = mat4(1.0);
+    uniform mat4 u_view = mat4(1.0);
+    uniform mat4 u_projection = mat4(1.0);
 
-void main() {
-    fragTexCoord = texCoord;
-    gl_Position = u_projection * u_view * u_model * vec4(position, 1.0f);
-}
+    void main() {
+        fragTexCoord = texCoord;
+        gl_Position = u_projection * u_view * u_model * vec4(position, 1.0f);
+    }
     """
-    frag_source = """
-#version 330
-in vec2 fragTexCoord;
+    frag_source = """ 
+    #version 330
+    in vec2 fragTexCoord;
 
-uniform sampler2D u_texture;
+    uniform sampler2D u_texture;
 
-out vec4 outColor;
+    out vec4 outColor;
 
-void main() {
-    outColor = texture(u_texture, fragTexCoord);
-}
+    void main() {
+        outColor = texture(u_texture, fragTexCoord);
+    }
     """
 
     # parte A
-    pipeline = ShaderProgram(Shader(vert_source, "vertex"), Shader(frag_source, "fragment"))
+    vertex_shader = Shader(vert_source, "vertex")
+    fragment_shader = Shader(frag_source, "fragment")
+    pipeline = ShaderProgram(vertex_shader, fragment_shader)
     root = os.path.dirname(__file__)
 
     cam = MyCam([0,0,2])
 
     world = SceneGraph(cam)
 
-    Tileset = Texture(root + "/TileSet.png", minFilterMode=GL_NEAREST, maxFilterMode=GL_NEAREST)
+    Tileset = Texture(root, image = "/TileSet.png", minFilterMode=GL_NEAREST, maxFilterMode=GL_NEAREST)
+    background = Texture(root, image = "/backgroundColor.png", minFilterMode=GL_NEAREST, maxFilterMode=GL_NEAREST, sWrapMode=GL_REPEAT, tWrapMode=GL_REPEAT)
+    background2 = Texture(root, image = "/backgroundShapes.png", minFilterMode=GL_NEAREST, maxFilterMode=GL_NEAREST, sWrapMode=GL_REPEAT, tWrapMode=GL_REPEAT)
 
     aW = Tileset.width
     aH = Tileset.height
+
+    bW = background.width
+    bH = background.height
+
+    fondo = [
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        1.0, 1.0
+    ]
+
+    paisaje = Model(shapes.Square["position"], fondo, index_data=shapes.Square["indices"])
+    world.add_node("root")
+    world.add_node("Fondo", attach_to="root", mesh=fondo, texture = background, pipeline=pipeline)
+    world.add_node("Fondo2", attach_to="root", mesh=fondo, texture = background2, pipeline=pipeline)
+    world.add_node("Player", attach_to="root")
+
 
     head = [
         0.655, 0.0,
@@ -97,11 +119,24 @@ void main() {
         0.790, 0.201,
         0.655, 0.201
     ]
+    body = [
+        0.790, 0.0,
+        1.0, 0.0,
+        1.0, 0.250,
+        0.790, 0.250
 
-    square = Model(shapes.Square["position"], head, index_data=shapes.Square["indices"])
+    ]
 
-    world.add_node("head", mesh=square, texture=Tileset, pipeline=pipeline)
+
+    cabeza = Model(shapes.Square["position"], head, index_data=shapes.Square["indices"])
+    cuerpo = Model(shapes.Square["position"], body, index_data=shapes.Square["indices"])
+
+    world.add_node("head", attach_to="Player", mesh=cabeza, texture=Tileset, pipeline=pipeline)
     world["head"]["scale"] = [1, 1.3, 1]
+    world["head"]["translate"] = tr.translate(0.0, 0.6, 0.0)
+    world.add_node("body", attach_to="Player", mesh=cuerpo, texture=Tileset, pipeline=pipeline)
+    world["body"]["scale"] = [1, 1.3, 1]
+    world["body"]["translate"] = tr.translate(0.0, -0.4, 0.0)
 
 
     @controller.event
@@ -135,11 +170,6 @@ void main() {
         if symbol == key.A or symbol == key.D:
             cam.direction[1] = 0
 
-    @controller.event
-    def on_mouse_motion(x, y, dx, dy):
-        cam.yaw += dx * .001
-        cam.pitch += dy * .001
-        cam.pitch = math.clamp(cam.pitch, -(np.pi/2 - 0.01), np.pi/2 - 0.01)
 
     #Informacion que se actualiza con el tiempo
     def update(dt):
